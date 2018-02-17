400d169e1f2be79ff8136fcae25b9bcd78c81819
Sagar Ghimire <s525189@nwmissouri.edu>
2018-02-16 18:54:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/s525189/eventtracker/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.s525189.eventtracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Intent i = getIntent();
+    }
+}
