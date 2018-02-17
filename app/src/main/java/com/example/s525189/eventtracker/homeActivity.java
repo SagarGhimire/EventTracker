0235e130089e00db0cc79f96a08e607119e765f0
Sumnimarana1 <sumnimarana1@gmail.com>
2018-02-16 23:52:33 -0600

    Make the navigator bar functioality and event page

--- app/src/main/java/com/example/s525189/eventtracker/homeActivity.java
+++ app/src/main/java/com/example/s525189/eventtracker/homeActivity.java
@@ -1,13 +1,43 @@
 package com.example.s525189.eventtracker;
 
+import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class homeActivity extends AppCompatActivity {
-
+    private TextView infoTextView;
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //infoTextView =(TextView) findViewById(R.id.infoTextView);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if (item.getItemId() == R.id.map) {
+
+                } else if (item.getItemId() == R.id.event) {
+
+                    Intent i = new Intent(homeActivity.this,event.class);
+                    startActivity(i);
+                }
+
+
+                else if (item.getItemId() == R.id.ven) {
+
+                } else if (item.getItemId() == R.id.pro) {
+
+                }
+
+                return false;
+            }
+        });
     }
+
 }
