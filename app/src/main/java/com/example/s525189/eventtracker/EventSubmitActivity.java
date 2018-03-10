412347edd22ab7762eead28a2e991dcf13f6fbfe
Sumnimarana1 <sumnimarana1@gmail.com>
2018-03-10 13:10:54 -0600

    Added the Admin view and Event Form to submit the form by admin

--- app/src/main/java/com/example/s525189/eventtracker/EventSubmitActivity.java
+++ app/src/main/java/com/example/s525189/eventtracker/EventSubmitActivity.java
@@ -9,5 +9,8 @@ public class EventSubmitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_submit);
+        Log.d("second access", "starting web action");
+
+
     }
 }
