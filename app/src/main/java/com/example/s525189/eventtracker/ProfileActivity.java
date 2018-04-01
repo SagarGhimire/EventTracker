bc9d5688bfd1a3bbff3cdcfcc950dd1a1333a781
Nista <s525389@nwmissouri.edu>
2018-03-31 20:58:15 -0500

    Made a listview in Profile layout and also adjusted some changes in xml pages.

--- app/src/main/java/com/example/s525189/eventtracker/ProfileActivity.java
+++ app/src/main/java/com/example/s525189/eventtracker/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.s525189.eventtracker;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -8,15 +9,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static java.security.AccessController.getContext;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -44,6 +57,31 @@ public class ProfileActivity extends AppCompatActivity {
 
 
                 return false;
+            }
+        });
+
+        final ArrayList<String> item = new ArrayList<>();
+        item.add("id");
+        item.add("First Name");
+        item.add("Last Name");
+
+
+        ListAdapter itemAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item) {
+            @NonNull
+            @Override
+            public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent) {
+               View v =super.getView(position,convertView,parent);
+               return v;
+            }
+
+
+        };
+        ListView myList = (ListView) findViewById(R.id.myList);
+        myList.setAdapter(itemAdapter);
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
             }
         });
     }
